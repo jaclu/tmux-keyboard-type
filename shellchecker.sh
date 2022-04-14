0e5cb96c8967aa15622b1ed9c59497e2844264d4
jaclu <jacob.lundqvist@gmail.com>
2022-04-14 12:24:40 +0200

    made it more informative

--- shellchecker.sh
+++ shellchecker.sh
@@ -1,20 +1,34 @@
-#!/bin/sh
+#!/usr/bin/env bash
 #
-#   Copyright (c) 2022: Jacob.Lundqvist@gmail.com
-#   License: MIT
+#  Copyright (c) 2022: Jacob.Lundqvist@gmail.com
+#  License: MIT
 #
-#   Part of https://github.com/jaclu/tmux-keyboard-type
+#  Part of https://github.com/jaclu/tmux-keyboard-type
 #
-#   Version: 1.0.0 2022-04-14
+#  Version: 1.0.1 2022-04-14
 #
-#   Does shellcheck on all relevant scripts in this project
+#  Does shellcheck on all relevant scripts in this project
 #
 
-# shellcheck disable=SC1007
+#  shellcheck disable=SC1007
 CURRENT_DIR=$(CDPATH= cd -- "$(dirname -- "$0")" && pwd)
 
-#  Obviousl self exam should be done :)
-shellcheck "$CURRENT_DIR"/shellchecker.sh
 
-shellcheck "$CURRENT_DIR"/keyboard_type.tmux
-shellcheck "$CURRENT_DIR"/scripts/*.sh
+cd "$CURRENT_DIR" || return
+
+checkables=(
+
+    #  Obviously self exam should be done :)
+    shellchecker.sh
+
+    keyboard_type.tmux
+
+    scripts/*.sh
+)
+
+for script in "${checkables[@]}"; do
+    #  abort as soon as one gives warnings
+    echo "Checking: $script"
+    shellcheck -x "$script" || exit 1
+
+done
